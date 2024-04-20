de1f85a6a415b962cf59fe9ebc9d48d289b81033
w-pollock <w.pollock@uniandes.edu.co>
2024-04-20 14:06:11 -0500

    modificacion temporal propietario

--- Entrega_2/src/usuarios/Propietario.java
+++ Entrega_2/src/usuarios/Propietario.java
@@ -1,7 +1,33 @@
 package usuarios;
 
+import java.util.List;
+
+import modelo.Pieza;
 import modelo.Usuario;
 
 public class Propietario extends Usuario{
 
+	private boolean esComprador;
+	private String telefono;
+	private String direccion;
+	
+	public String idUsuario;
+	public String contrasenia;
+	public String nombre;
+	
+	public Propietario(String idUsuario, String contrasenia, String nombre) {
+		this.idUsuario = idUsuario;
+		this.contrasenia = contrasenia;
+		this.nombre = nombre;
+	}
+	
+	public List<Pieza> consultarPiezasBajoDominio(){
+		return null;
+		//TODO: FALTA CREAR METODO
+	}
+	
+	public List<Pieza> consultarPiezasAntiguas(){
+		return null;
+		//TODO: FALTA CREAR METODO
+	}
 }
