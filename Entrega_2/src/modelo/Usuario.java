11399665d8ba9b3315460281632593ffff58fcb1
w-pollock <w.pollock@uniandes.edu.co>
2024-04-20 14:24:33 -0500

    modificacion temporal usuario

--- Entrega_2/src/modelo/Usuario.java
+++ Entrega_2/src/modelo/Usuario.java
@@ -2,4 +2,17 @@ package modelo;
 
 public abstract class Usuario {
 
+	protected String idUsuario;
+	protected String contrasenia;
+	protected String nombre;
+	
+	protected Usuario(String idUsuario, String contrasenia, String nombre) {
+		this.idUsuario = idUsuario;
+		this.contrasenia = contrasenia;
+		this.nombre = nombre;
+	}
+	
+	protected void login() {
+		//TODO: FALTA CREAR METODO
+	}
 }
