b752a0e4a4254fd30e6d5b6df6520a5a58175b2d
w-pollock <w.pollock@uniandes.edu.co>
2024-04-20 13:44:27 -0500

    modificaciones temporales cliente

--- Entrega_2/src/usuarios/Administrador.java
+++ Entrega_2/src/usuarios/Administrador.java
@@ -1,7 +1,40 @@
 package usuarios;
 
+import java.util.Map;
+
+import modelo.Pieza;
 import modelo.Usuario;
 
 public class Administrador extends Usuario {
 
+	public String idUsuario;
+	public String contrasenia;
+	public String nombre;
+	
+	public Administrador(String idUsuario, String contrasenia, String nombre) {
+		this.idUsuario = idUsuario;
+		this.contrasenia = contrasenia;
+		this.nombre = nombre;
+	}
+	
+	public void crearSubasta(Map<String, Cliente> clientes, Map<String, Pieza> piezas) {
+		//TODO: FALTA CREAR MÉTODO
+	}
+	
+	public void registrarPieza() {
+		//TODO: FALTA CREAR MÉTODO
+	}
+	
+	public void confirmarVenta() {
+		//TODO: FALTA CREAR MÉTODO
+	}
+	
+	public void regresarPieza() {
+		//TODO: FALTA CREAR MÉTODO
+	}
+	
+	public int aumentarNumComprasCliente(Cliente cliente) {
+		return 0;
+		//TODO: FALTA CREAR MÉTODO
+	}
 }
