23e56d2c4525bee3ce5ffc2a83e5aebc64d7b189
w-pollock <w.pollock@uniandes.edu.co>
2024-04-20 13:48:10 -0500

    modificacion temporal cajero

--- Entrega_2/src/usuarios/Cajero.java
+++ Entrega_2/src/usuarios/Cajero.java
@@ -1,7 +1,22 @@
 package usuarios;
 
+import modelo.Pieza;
 import modelo.Usuario;
 
 public class Cajero extends Usuario {
 
+	public String idUsuario;
+	public String contrasenia;
+	public String nombre;
+	
+	public Cajero(String idUsuario, String contrasenia, String nombre) {
+		this.idUsuario = idUsuario;
+		this.contrasenia = contrasenia;
+		this.nombre = nombre;
+	}
+	
+	public void generarPago(Cliente cliente, Pieza piezas) {
+		//TODO: FALTA CREAR MÉTODO!
+	}
+	
 }
