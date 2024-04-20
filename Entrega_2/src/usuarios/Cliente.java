4061ad61ed18ef5e8a814198fd46a083a246f49f
w-pollock <w.pollock@uniandes.edu.co>
2024-04-20 14:11:21 -0500

    modificacion temporal cliente

--- Entrega_2/src/usuarios/Cliente.java
+++ Entrega_2/src/usuarios/Cliente.java
@@ -4,4 +4,23 @@ import modelo.Usuario;
 
 public class Cliente extends Usuario{
 
+	private boolean esPropietario;
+	private String telefono;
+	private String direccion;
+	private int numCompras;
+	
+	public String idUsuario;
+	public String contrasenia;
+	public String nombre;
+	
+	public Cliente(String idUsuario, String contrasenia, String nombre) {
+		this.idUsuario = idUsuario;
+		this.contrasenia = contrasenia;
+		this.nombre = nombre;
+	}
+	
+	
+	public void setNumCompras(int numeroNuevo) {
+		//TODO: FALTA CREAR METODO
+	}
 }
