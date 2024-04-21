3d3acf0d99ef34c7bf53246622c12168930ba6e0
w-pollock <w.pollock@uniandes.edu.co>
2024-04-20 23:08:09 -0500

    mod operador

--- Entrega_2/src/usuarios/Operador.java
+++ Entrega_2/src/usuarios/Operador.java
@@ -1,5 +1,9 @@
 package usuarios;
 
+import java.util.HashMap;
+
+import modelo.Historial;
+import modelo.Pieza;
 import modelo.Usuario;
 
 public class Operador extends Usuario{
@@ -12,8 +16,10 @@ public class Operador extends Usuario{
 		super(idUsuario, contrasenia, nombre);
 	}
 	
-	public void crearHistorial(String tituloPieza) {
-		//TODO: FALTA CREAR METODO!
+	public void crearHistorial(String tituloPieza, String idSubasta, Pieza pieza,
+			int valorPuja, int idComprador) {
+		Historial historial = new Historial(idSubasta, pieza, new HashMap<>(), valorPuja, idComprador);
+		historial.agregarAlMapa(valorPuja, idSubasta);
 	}
 	
 	public void aniadirPuja(String idComprador, int valor) {
