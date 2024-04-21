9bd1d32e84cdfdd8ff955fe6547d64fb3ea193c7
ctellez22 <142060772+ctellez22@users.noreply.github.com>
2024-04-20 23:04:42 -0500

    11:04

--- Entrega_2/src/modelo/Subasta.java
+++ Entrega_2/src/modelo/Subasta.java
@@ -4,31 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Subasta {
-	
-	
-	private String idSubasta;
-	private Pieza pieza;
-	
-	
-	
-	
-	public Subasta(String idSubasta, Pieza pieza) {
+    private int idSubasta;
+    private List<Pieza> piezas;
+    private List<Historial> historiales;
 
-		this.idSubasta = idSubasta;
-		this.pieza = pieza;
-	}
-	
-	
-	
-	
-	
-			
-	
-	
-	
+    public Subasta(int idSubasta, Pieza pieza, Historial historial) {
+        this.idSubasta = idSubasta;
+        this.piezas = new ArrayList<>();
+        this.historiales = new ArrayList<>();
 
+        this.piezas.add(pieza);
+        this.historiales.add(historial);
+    }
+
+    public List<Pieza> getPiezas() {
+        return piezas;
+    }
+
+    public List<Historial> getHistoriales() {
+        return historiales;
+    }
+
+    public void addPieza(Pieza pieza) {
+        this.piezas.add(pieza);
+    }
+
+    public void removePieza(Pieza pieza) {
+        this.piezas.remove(pieza);
+    }
+
+    public void addHistorial(Historial historial) {
+        this.historiales.add(historial);
+    }
+
+    public void removeHistorial(Historial historial) {
+        this.historiales.remove(historial);
+    }
 }
 
-
+		
 
 
